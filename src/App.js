c0afcacc8bfb33acc620d2ec55138eb6c718e27e
CosmicLatte009 <ohhm009@gmail.com>
2022-12-21 21:10:39 +0900

    chore: resultPage meta 태그 react-helmet-async로 수정

--- src/App.js
+++ src/App.js
@@ -2,48 +2,17 @@ import HomePage from "./pages/HomePage";
 import QuestionPage from "./pages/QuestionPage";
 import ResultPage from "./pages/ResultPage";
 import AllResultPage from "./pages/AllResultPage";
-import favicon from "./assets/svg/favicon.svg";
 import { Routes, Route } from "react-router-dom";
-import { Helmet } from "react-helmet-async";
 import "./style/commonStyle/reset.css";
 
 function App() {
 	return (
-		<>
-			<Helmet>
-				<meta
-					name="naver-site-verification"
-					content="40a12bac3fad1a6b0bddfbd55952079d48ef2a12"
-				/>
-				<meta property="og:type" content="website" />
-				<meta property="og:title" content="크리스마스 질투 유형 테스트" />
-				<meta
-					property="og:description"
-					content="연애할 때 내 질투의 정도를 알아보는 재미용 심리 테스트입니다."
-				/>
-				<meta property="og:image" content="./assets/svg/logo.svg" />
-				<meta
-					property="og:url"
-					content="https://christmas-mbti-test.netlify.app"
-				/>
-				<link rel="icon" href={favicon} />
-				<link rel="apple-touch-icon" href={favicon} />
-				<meta
-					name="description"
-					content="연애할 때 내 질투의 정도를 알아보는 재미용 심리 테스트입니다."
-				/>
-				<meta charset="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<title>크리스마스 질투 유형 테스트</title>
-			</Helmet>
-
-			<Routes>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/question" element={<QuestionPage />} />
-				<Route path="/result" element={<ResultPage />} />
-				<Route path="/allResult" element={<AllResultPage />} />
-			</Routes>
-		</>
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			<Route path="/question" element={<QuestionPage />} />
+			<Route path="/result" element={<ResultPage />} />
+			<Route path="/allResult" element={<AllResultPage />} />
+		</Routes>
 	);
 }
 
--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -8,6 +8,7 @@ import Guage from "../components/guage/Guage";
 import ResultDesc from "../components/resultDesc/ResultDesc";
 import Button from "../components/common/Button";
 import ShareButton from "../components/shareButton/ShareButton.jsx";
+import { Helmet } from "react-helmet-async";
 import styles from "../style/pagesStyle/ResultPage.module.scss";
 
 const ResultPage = () => {
@@ -17,6 +18,9 @@ const ResultPage = () => {
 
 	const [resultData, setResultData] = useState({});
 
+	const resultUrl = `https://christmas-mbti-test.netlify.app/result?mbti=${mbti}`;
+	const ogDesc = `당신은 "${resultData.name}" 유형입니다`;
+
 	useEffect(() => {
 		window.scrollTo(0, 0);
 		const result = ResultData.find((s) => s.type === mbti);
@@ -24,26 +28,39 @@ const ResultPage = () => {
 	}, [mbti]);
 
 	return (
-		<Wrapper>
-			<Bow className={styles["bow-icon"]} />
-			<h1 className={styles.title}>{resultData.name}</h1>
-			<img src={resultData.image} alt="결과 이미지" width={300}></img>
-			<Guage resultData={resultData} />
-			<ResultDesc resultData={resultData} />
-			<Match resultData={resultData} />
-			<div className={styles["btn-group"]}>
-				<Button
-					bold
-					onClick={() => navigate("/allResult", { state: resultData })}
-				>
-					내 유형 질투력 순위 보기
-				</Button>
-				<Button bold onClick={() => navigate("/")}>
-					테스트 다시하기
-				</Button>
-				<ShareButton resultData={resultData} />
-			</div>
-		</Wrapper>
+		<>
+			<Helmet>
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="내 크리스마스 질투 테스트 결과" />
+				<meta property="og:description" content={ogDesc} />
+				<meta property="og:image" content={resultData.image} />
+				<meta property="og:url" content={resultUrl} />
+				<meta name="description" content={ogDesc} />
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<title>내 크리스마스 질투 테스트 결과</title>
+			</Helmet>
+			<Wrapper>
+				<Bow className={styles["bow-icon"]} />
+				<h1 className={styles.title}>{resultData.name}</h1>
+				<img src={resultData.image} alt="결과 이미지" width={300}></img>
+				<Guage resultData={resultData} />
+				<ResultDesc resultData={resultData} />
+				<Match resultData={resultData} />
+				<div className={styles["btn-group"]}>
+					<Button
+						bold
+						onClick={() => navigate("/allResult", { state: resultData })}
+					>
+						내 유형 질투력 순위 보기
+					</Button>
+					<Button bold onClick={() => navigate("/")}>
+						테스트 다시하기
+					</Button>
+					<ShareButton resultData={resultData} />
+				</div>
+			</Wrapper>
+		</>
 	);
 };
 
