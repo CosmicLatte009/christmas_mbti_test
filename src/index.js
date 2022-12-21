7ba9d7baa0b163f7be50130739cbd5fd2a29846b
CosmicLatte009 <ohhm009@gmail.com>
2022-12-21 18:49:56 +0900

    chore: react-helmet 재설치, 디폴트 메타태그 App.js에 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Snowfall from "react-snowfall";
+import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
@@ -8,14 +9,16 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Snowfall
-				snowflakeCount={150}
-				speed={[0.6, 0.8]}
-				wind={[0.3, 0.3]}
-				radius={[0.5, 2]}
-			/>
-			<App />
-		</BrowserRouter>
+		<HelmetProvider>
+			<BrowserRouter>
+				<Snowfall
+					snowflakeCount={150}
+					speed={[0.6, 0.8]}
+					wind={[0.3, 0.3]}
+					radius={[0.5, 2]}
+				/>
+				<App />
+			</BrowserRouter>
+		</HelmetProvider>
 	</React.StrictMode>
 );
