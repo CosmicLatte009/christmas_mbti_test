1ea4da40d8f4ffc6335ef8830698025054ff29ec
CosmicLatte009 <ohhm009@gmail.com>
2024-04-29 02:27:37 +0900

    perf: fcp 개선을 위해 lazy import 대신 직접 imprt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React, { lazy } from "react";
-
 import { Routes, Route } from "react-router-dom";
 import "./style/commonStyle/reset.css";
-
-const HomePage = lazy(() => import("./pages/HomePage"));
-const QuestionPage = lazy(() => import("./pages/QuestionPage"));
-const ResultPage = lazy(() => import("./pages/ResultPage"));
-const AllResultPage = lazy(() => import("./pages/AllResultPage"));
+import HomePage from "./pages/HomePage";
+import QuestionPage from "./pages/QuestionPage";
+import ResultPage from "./pages/ResultPage";
+import AllResultPage from "./pages/AllResultPage";
 
 function App() {
 	return (
